8884ed67a7a2eddebc226be3b6384db2a2599fe3
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-23 17:24:31 +0530

    Payments endpoint added

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ app.config["JWT_BLACKLIST_TOKEN_CHECK"] = [
 app.secret_key = "visa"
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 @app.errorhandler(ValidationError)
@@ -43,8 +46,9 @@ api.add_resource(User, "/user/<string:mobile_number>")
 api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(VirtualCard, "/virtual_card/<string:mobile_number>")
 api.add_resource(AddAmount, "/virtual_card/add_amount/<string:mobile_number>")
-# api.add_resource(Payment, "/virtual_card/payment/<string:mobile_number>")
+api.add_resource(Payment, "/virtual_card/payment/<string:mobile_number>")
 
 if __name__ == "__main__":
+    db.init_app(app)
     ma.init_app(app)
     app.run(port=5001,debug=True)
