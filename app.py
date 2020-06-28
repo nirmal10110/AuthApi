6513444bbcd39466cc758d85656ceda1b2853c83
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-28 18:28:27 +0530

    .

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def check_if_token_in_blacklist(decrypted_token):
 api.add_resource(UserRegister, "/register")
 api.add_resource(UserLogin, "/login")
 api.add_resource(UserLogout, "/logout")
-api.add_resource(User, "/user/<string:mobile_number>")
+api.add_resource(User, "/user")
 api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(UserConfirm, "/otp")
 api.add_resource(VirtualCard, "/virtual_card")
